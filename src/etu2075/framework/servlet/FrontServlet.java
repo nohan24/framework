4a099c2a6b979aaed6e47e63d05f21f25774d001
Nohan24 <94552970+nohan24@users.noreply.github.com>
2023-03-07 09:07:15 +0300

    frontServlet

--- src/etu2075/framework/servlet/FrontServlet.java
+++ src/etu2075/framework/servlet/FrontServlet.java
@@ -1,27 +1,18 @@
-package process;
+package etu2075.framework.servlet;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import database.*;
 import java.io.*;
-import java.sql.Connection;
 
-public class ProcessRequestServlet extends HttpServlet{
+public class FrontServlet extends HttpServlet{
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
         res.setContentType("text/plain");
         PrintWriter out = res.getWriter();
         out.println(req.getRequestURI());
-        try {
-            Connection c = Postgres.connect();
-            out.println(c);
-            c.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
     }
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
         res.setContentType("text/plain");
         PrintWriter out = res.getWriter();
         out.println(req.getRequestURI());
     }
-
 }
