189b35120ec4eaedea1a7f03508e84060ef94980
Nohan24 <94552970+nohan24@users.noreply.github.com>
2023-03-07 09:56:28 +0300

    sprint2

--- src/etu2075/framework/servlet/FrontServlet.java
+++ src/etu2075/framework/servlet/FrontServlet.java
@@ -1,22 +1,19 @@
 package etu2075.framework.servlet;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import etu2075.framework.Mapping;
 import java.io.*;
-import java.util.HashMap;
 
 public class FrontServlet extends HttpServlet{
-    HashMap<String,Mapping> MappingUrls;
+    protected void processRequest(HttpServletRequest req,HttpServletResponse res) throws IOException{
+        res.setContentType("text/plain");
+        PrintWriter out = res.getWriter();
+        out.println(req.getRequestURI());
+    }
 
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
-        res.setContentType("text/plain");
-        PrintWriter out = res.getWriter();
-        out.println(req.getRequestURI());
-
+        processRequest(req, res);
     }
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
-        res.setContentType("text/plain");
-        PrintWriter out = res.getWriter();
-        out.println(req.getRequestURI());
+        processRequest(req, res);
     }
 }
