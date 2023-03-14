0597358c928292086047ca286304f20e5b96ea64
nohan24 <nohanrazafinjato@gmail.com>
2023-03-14 15:30:37 +0300

    test

---
+++ src/base/Emp.java
@@ -0,0 +1,7 @@
+package base;
+
+public class Emp {
+    public String insert(){
+        return "insertion";
+    }
+}
--- src/etu2075/framework/servlet/FrontServlet.java
+++ src/etu2075/framework/servlet/FrontServlet.java
@@ -4,9 +4,35 @@ import javax.servlet.http.*;
 import etu2075.framework.Mapping;
 import java.io.*;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class FrontServlet extends HttpServlet{
     HashMap<String,Mapping> urlMapping;
+
+    public Set<Class> findAllClassesUsingClassLoader(String packageName) {
+        InputStream stream = ClassLoader.getSystemClassLoader()
+          .getResourceAsStream(packageName.replaceAll("[.]", "/"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        return reader.lines().filter(line -> line.endsWith(".class")).map(line -> getClass(line, packageName)).collect(Collectors.toSet());
+    }
+ 
+    private Class getClass(String className, String packageName) {
+        try {
+            return Class.forName(packageName + "." + className.substring(0, className.lastIndexOf('.')));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public void init() throws ServletException {
+        Set<Class> classes = findAllClassesUsingClassLoader("base");
+        
+
+    }
+
     protected void processRequest(HttpServletRequest req,HttpServletResponse res) throws IOException{
         res.setContentType("text/plain");
         PrintWriter out = res.getWriter();
